a27383374b42cc898eaa7396678c7c27c26f6402
kohei <3173010@stu.hus.ac.jp>
2020-02-01 14:37:10 +0900

    Fix output GitHub

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,14 +1,14 @@
 class Fizzbuzz
   def calculate number
     case
+    when number.to_s.include?('7')
+      'GitHub'
     when number % 3 == 0 && number % 5 == 0
       'fizzbuzz'
     when number % 3 == 0
       'fizz'
     when number % 5 == 0
       'buzz'
-    when number.to_s.include?('7')
-      'GitHub'
     else
       number
     end
